3fcf676e467dfd8d10c1f59ce6c5705caf9b237b
DragonVortex <ztmdavis@sbcglobal.net>
2018-01-27 12:26:25 -0500

    Audio Change

--- src/21.js
+++ src/21.js
@@ -22,7 +22,7 @@ const steps = 200
 const segments = isSafari ? 50 : 100
 const radius = 0.1
 const thickness = 0.01
-const src = 'assets/*.m4a'
+const src = 'assets/*.mp3'
 
 const defaults = {
   opacity: 0.5,
